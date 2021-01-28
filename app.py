da93475d1bb9130864d833b58338bfa34f51a8cf
glitch <saniltison@gmail.com>
2021-01-27 19:54:55 -0500

    commets were added to source code to explain what is going on

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+# this is a route that tell flask how to get to this page and display it.
+# at the browser when [127.0.0.1:5000/] is entered flask will call upon the 
+# "home" python function.
+# that "home" python function will then do what ever that is in the return section
+# of the function.
+# in this case it will return the text " search" & "update" with html tags
+# search
+# update
 @app.route("/")
 def home():
     return "<p>search</p> <p>update</p>"
